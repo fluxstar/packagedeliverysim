3bdab96ec6f5c8431c93327dcce444f21a86f152
Logan Krant <krant115@umn.edu>
2024-04-12 14:37:45 -0500

    Added Doxyen comments to auction header files

--- service/include/simulationmodel/auction/AuctionSimulationModel.h
+++ service/include/simulationmodel/auction/AuctionSimulationModel.h
@@ -1,11 +1,20 @@
 #ifndef AUCTIONSIMULATIONMODEL_H_
 #define AUCTIONSIMULATIONMODEL_H_
 
-#include "SimulationModel.h"
 #include "Auctioneer.h"
 
+//--------------------  AuctionSimulationModel ----------------------------
+
+/**
+ * @brief Class AuctionSimulationModel wraps the SimulationModel class and overrides
+ * the update method to handle the auction simulation, managing an Auctioneer object
+*/
 class AuctionSimulationModel : public SimulationModel {
  public:
+  /**
+   * @brief Update the simulation model
+   * @param dt Type double containing the time since update was last called.
+  */
   void update(double dt) override;
  private:
   Auctioneer auctioneer;
--- service/include/simulationmodel/auction/Auctioneer.h
+++ service/include/simulationmodel/auction/Auctioneer.h
@@ -1,12 +1,33 @@
 #ifndef AUCTIONEER_H_
 #define AUCTIONEER_H_
 
+#include <vector>
 #include "SimulationModel.h"
 #include "Package.h"
+#include "AuctionDrone.h"
 
+//--------------------  Auctioneer ----------------------------
+
+/**
+ * @brief Class Auctioneer manages the auctioning of drones to packages
+ * in the simulation using wait times and available drones
+ **/
 class Auctioneer : public SimulationModel {
  public:
+  /**
+   * @brief Default constructor that creates the Auctioneer object
+  */
+
   Auctioneer(SimulationModel* model, int maxWait);
+  /**
+   * @brief Destructor
+  */
+
+  ~Auctioneer();
+  /**
+   * @brief Update the auction
+   * @param dt Type double containing the time since update was last called.
+  */
   void update(double dt);
  private:
   std::vector<AuctionDrone*> availableDrones;
